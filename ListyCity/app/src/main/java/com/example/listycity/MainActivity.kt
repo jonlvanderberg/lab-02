79badc1a1c792097c2d1c4faaf07858216c59310
jonlvanderberg <jlvander@ualberta.ca>
2026-09-11 13:19:26 -0600

    ListyCity lab demo

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.kt
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -4,30 +4,90 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val cityRepository = CityRepository()
         setContent {
             ListyCityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CityListScreen(
+                        cities = cityRepository.cities,
+                        onAddCity = {cityRepository.addCity( it )},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+@Composable
+fun CityListScreen(
+    cities: List<String>,
+    onAddCity: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var newCityName by remember { mutableStateOf(value = "") }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Row() {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = {newCityName = it },
+                label = {Text("City Name")},
+                modifier =Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick =  {
+                if (newCityName.isNotBlank()) {
+                    onAddCity(newCityName)
+                    newCityName
+                }
+            }) {Text("Add City") }
+        }
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            items(cities){
+                    city -> CityRow(city=city)
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun CityRow(city: String) {
+    Text(
+        text = city,
+        fontSize = 28.sp,
+        modifier = Modifier.fillMaxWidth().padding(horizontal=18.dp, vertical=14.dp)
+    )
 }
 
 @Composable
@@ -43,5 +103,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ListyCityTheme {
         Greeting("Android")
+    }
+}
+
+class CityRepository {
+    private val _cities = mutableStateListOf("Edmonton", "Vancouver", "Moscow", "Sydney",
+        "Berlin", "Vienna", "Tokyo", "Beijing")
+
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String) {
+        _cities.add(city)
     }
 }
